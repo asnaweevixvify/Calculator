86683f50cd8641e22d21b71e000abac639d021b5
asnaweevixvify <asnaweeezor@gmail.com>
2025-03-31 19:33:44 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function appendNum(numInput){
+    let Display = document.querySelector('#display');
+    Display.value += numInput;
+}
+function equal(){
+    let Display = document.querySelector('#display');
+    let finalNum=Display.value;
+    let answer = eval(finalNum);
+    Display.value = answer
+}
+
+
